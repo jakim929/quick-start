4ebe02a71bd84776a5b6861770df30bc39a16689
James Kim <jakim929@gmail.com>
2024-10-13 14:37:41 -0700

    repro: remove return function in writeFile

--- contracts/Fs.sol
+++ contracts/Fs.sol
@@ -8,17 +8,12 @@ interface Fs {
      * @param path The path of the file to read.
      * @return data The content of the file.
      */
-    function readFile(string calldata path)
-        external
-        view
-        returns (string memory data);
+    function readFile(string calldata path) external view returns (string memory data);
 
     /**
      * @notice Writes data to a file at the specified path.
      * @param path The path of the file to write to.
      * @param data The data to write to the file.
      */
-    function writeFile(string calldata path, string calldata data)
-        external
-        returns (bool success);
+    function writeFile(string calldata path, string calldata data) external;
 }
